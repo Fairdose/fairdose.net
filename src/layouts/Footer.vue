577e69339a6ca15f37f091c9941a2c5cb070b5ff
Batur <fairdose@cyberservices.com>
2022-03-02 20:12:16 +0300

    - Added styles, fonts - Worked on cli footer mainly

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+
+const pwd = {
+  path: `guest@fairdose.net:${route.fullPath}$`,
+  command: ''
+}
+
+</script>
+
+<template>
+  <div id="fr-cli">
+    <span class="">{{ pwd.path }}</span>
+    <input v-model="pwd.command" />
+  </div>
+</template>
+
+<style lang="scss">
+@use 'src/assets/style/colors';
+#fr-cli {
+  padding: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: calc(100% - 2em);
+  & > input {
+    background: rgba(0, 0, 0, 0);
+    color: transparent;
+    text-shadow: 0 0 0 colors.$m-white;
+    width: calc(100% - 2em);
+    box-sizing: border-box;
+    font-size: 1rem;
+    border: none;
+    resize: none;
+    position: relative;
+    transform: translate(5px,-1px);
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
