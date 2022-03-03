4ea25c56255bba84fd31ea39d0ba05681c88b222
Batur <fairdose@cyberservices.com>
2022-03-03 16:09:21 +0300

    New views added
    - About, Stack, Contact, 404 pages added

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <div id="fr-view">
       <RouterView/>
     </div>
-    <Footer />
+    <Footer @command="commandResult"/>
   </div>
 </template>
 
@@ -13,6 +13,10 @@
 import { RouterView } from 'vue-router'
 import Header from "./layouts/Header.vue";
 import Footer from "./layouts/Footer.vue";
+
+const commandResult = (cmd) => {
+  console.log(cmd)
+}
 </script>
 
 <style lang="scss">
